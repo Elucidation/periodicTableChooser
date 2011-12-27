3a50272e2dbf7ea5b631f1633b97ac5b95b99f51
Elucidation <elucidation@gmail.com>
2011-12-26 21:52:31 -0700

    working jquery/php/mysql autocomplete for periodic table elements

--- hello.php
+++ hello.php
@@ -4,5 +4,15 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
+ <p>Blah</p>
+ <?php 
+ 	$data = array();
+ 	$data[] = "Yo";
+ 	$data[] = "Boop";
+ 	echo json_encode($data);
+ ?>
+ <p>And Info:</p>
+ <?php phpinfo(); ?>
+
  </body>
 </html>
--- periodic-list.php
+++ periodic-list.php
@@ -4,5 +4,32 @@
 if ( !isset($_REQUEST['term']) )
 	exit;
 
-echo json_encode(["bloop","abpc"]);
+$dblink = mysql_connect('localhost','testuser','testpwd','sam1') or die( 'Could not connect: '. mysql_error() );
+
+mysql_select_db('sam1');
+
+$rs = mysql_query('
+SELECT id,name,symbol 
+FROM PeriodicTable
+WHERE name like "%'. mysql_real_escape_string($_REQUEST['term']) .'%" 
+OR id = "'. mysql_real_escape_string($_REQUEST['term']) .'"
+order by name
+LIMIT 0,10
+');
+
+$data = array();
+
+if ( $rs && mysql_num_rows($rs) )
+{
+	while ( $row = mysql_fetch_array($rs, MYSQL_ASSOC) )
+	{
+		$data[] = array(
+				// Hg - Mercury
+				'label' => $row['symbol'] .' - '. $row['name'] . ' (#' . $row['id'] .')',
+				'value' => $row['name']
+			);
+	}
+}
+
+echo json_encode($data);
 flush();
